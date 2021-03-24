239b4873f37d5e0e1e30b0cab957770400f61de5
meriembelbari19 <77324128+meriembelbari19@users.noreply.github.com>
2021-03-24 18:22:57 +0100

    Update main.py

--- c16_03/main.py
+++ c16_03/main.py
@@ -1,4 +1,7 @@
 
+
+import statistics
+
 note1 = ('eleve1', 'math', 13)
 note2 = ('eleve1', 'physique', 15)
 note3 = ('eleve1', 'math', 13)
@@ -9,31 +12,24 @@ note7 = ('eleve2', 'math', 13)
 note8 = ('eleve2', 'math', 14)
 
 notes = [note1, note2, note3, note4, note5, note6,note7,note8]
+print(notes)
+
+notes_elv = [note for note in notes if note[0] == "eleve1"]
+print(notes_elv)
+notes_elv_matiere = [note for note in notes_elv if note[1] == "math"]
+print(notes_elv_matiere)
+note_eleve = [i[2] for i in notes_elv ]
+note_eleve_matiere= [i[2] for i in notes_elv_matiere ]
+print(note_eleve_matiere)
+print(statistics.mean(note_eleve_matiere))
 
 
-#moy = (sum(notes[3] for i in notes)/len(notes))
-#i=0
-#yaya = sum(notes[3] for i[2] in notes)
-#print(yaya)
+def moyenne_tuple(notes, eleve=None, matiere=None):
+    
+    notes_elv = [note for note in notes if note[0] == eleve] if eleve is not None else notes
+    notes_elv_matiere = [n for n in notes_elv if n[1] == matiere] if matiere is not None else notes_elv
+    return sum([n[2] for n in notes_elv_matiere])/len(notes_elv_matiere)
 
-#ok = sum(note[2] for note in notes)
-#print(ok)
-#print(notes[0][2])
-
-#pour l'élève 1 
-notess = notes[0:5]
-moy = (sum(note[2] for note in notess)/len(notess))
-print(moy)
-
-
-#pour les maths 
-newliste = notes[0],notes[5],notes[6],notes[7]
-print(newliste)
-moymaths = (sum(note[2] for note in newliste)/len(newliste))
-print(moymaths)
-
-
-#fonction moyenne tuple
 
 class Note:
   def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
@@ -45,16 +41,22 @@ class Note:
   def afficher(self):
     print('eleve', self.eleve, 'matiere', self.matiere, 'note', self.valeur)
 
+  def __str__(self):
+    return f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')" 
+
 
 onote = Note('eleve1', 'maths', 13)
 print(onote.eleve)
 print(onote.matiere)
 print(onote.valeur)
 Note.afficher(onote)
- 
 
 
+onote1 = [Note(eleve, matiere, valeur) for eleve, matiere, valeur in notes]
+for onote in onote1:
+  onote.afficher()
 
-
+for onote in onote1:
+  print(onote)
 
 
